03294d36175de7c8573b7760e71258b1344ce5a2
Dolores <dolores.macauley@gmail.com>
2020-06-16 10:53:14 -0400

    first commit to branch

--- src/App.js
+++ src/App.js
@@ -22,15 +22,6 @@ class App extends Component {
       return (
         <div className="App">
           <header className="App-header">
-            <video src="https://storage.coverr.co/videos/coverr-philippines-spices-1559212666899?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjExNDMyN0NEOTRCMUFCMTFERTE3IiwiaWF0IjoxNTY1MzQ0ODE5LCJleHAiOjE1NjUzNDg0MTl9.iC8X_4Sjw7xaGSpAq_Uw_Dv2hlM0z4bjjH1bPGmF_24"
-             poster ="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-                style={{
-                  objectFit: 'cover',
-                  width: '100%',
-                  height: '100vh',
-                }} loop autoplay="true"
-            />
-
             <Nav/>
           </header>
 
