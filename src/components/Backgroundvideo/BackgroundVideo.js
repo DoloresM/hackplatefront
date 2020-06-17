0e19e896dc4f1c60ad8f72f514d86d5143e83b0c
Dolores <dolores.macauley@gmail.com>
2020-06-16 23:17:58 -0400

    video background

--- src/components/Backgroundvideo/BackgroundVideo.js
+++ src/components/Backgroundvideo/BackgroundVideo.js
@@ -5,8 +5,8 @@ import classes from './BackgroundVideo.css';
 const BackgroundVideo = () => {
     const videoSource = "https://vod-progressive.akamaized.net/exp=1592368868~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4404%2F13%2F347024091%2F1397843845.mp4~hmac=fb41ef3cbbe0d934848518725a0788156b32533153ea765d5937ea60f8922283/vimeo-prod-skyfire-std-us/01/4404/13/347024091/1397843845.mp4?filename=Food+-+24999.mp4"
     return (
-        <div style={{background: "green", minWidth: "100%", minHeight: "100vh", margin: "0",padding: "0"}} >
-            <video autoPlay="autoplay" loop="loop" muted className={classes.Video} controls >
+        <div  >
+            <video autoPlay="autoplay" loop="loop" muted  style={{background: "green", minWidth: "100%", minHeight: "100vh", margin: "0",padding: "0" }} >
                 <source src={videoSource} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
