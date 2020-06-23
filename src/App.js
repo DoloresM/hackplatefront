f87b1000c36e5afe367e71224dcbc22a8af7a3c3
Dolores <dolores.macauley@gmail.com>
2020-06-23 15:25:33 -0400

    removing import background video bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Footer from "./components/Footer";
 import Body from "./components/Body";
 import Nav from "./components/Nav";
 import {Link} from "react-router-dom";
-import BackgroundVideo from "./components/Backgroundvideo/BackgroundVideo";
 import Heading from "./components/Heading"
 
 import './App.css';
@@ -13,12 +12,9 @@ import './App.css';
 const GIFT = process.env.REACT_APP_API_KEY;
 
 class App extends Component {
-
   constructor(){
     super()
-
   }
-
   componentDidMount(){
     this.videos = this.getVideo();
   }
